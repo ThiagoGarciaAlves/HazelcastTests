2162f14507488218a2cd15fbecf2a7b8ab55b234
Thiago G. Alves <thiago.alves@icolabora.com.br>
2016-05-13 11:50:06 -0300

    Criando o cluster

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,8 +1,14 @@
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
 /**
  * @author Thiago Alves
  */
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("HazelcastTests START");
+        Config cfg = new Config();
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
     }
 }
